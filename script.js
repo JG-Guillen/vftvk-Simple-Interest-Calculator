b12ae5b00809d07cb09671cf9293e8a261341d9e
JG-Guillen <38336339+JG-Guillen@users.noreply.github.com>
2021-11-11 00:33:50 -0400

    Updates

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ function displayvalues()
     var revenue = document.getElementById("display_revenue");
     fecha = new Date().getFullYear()
     revenue_date = Number(fecha) + Number(years.value)
-    //alert(revenue_date)
     revenue.innerHTML = "If you deposit " +amount.value+",<br>"+ "at an interest rate of " +rate.value+"%.<br>"+"You will receive an amount of " + amount.value*rate.value*years.value*0.01 +",<br>"+"In the Year "+ revenue_date
-    //alert("amount" + years.value);
     return false;
 }
         
@@ -17,6 +15,5 @@ function displayrange()
     var rate = document.getElementById("rate");
     var displayrate = document.getElementById("displayrate");
     displayrate.innerHTML = rate.value
-    //alert("amount" + years.value);
     return false;
 }
